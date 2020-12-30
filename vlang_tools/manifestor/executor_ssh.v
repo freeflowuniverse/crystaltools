08706139241e1deeb64bea19c96f4f735b411a57
hamdy <hamdy.a.farag@gmail.com>
2020-12-30 12:18:19 +0200

    fixes to ssh executor

--- vlang_tools/manifestor/executor_ssh.v
+++ vlang_tools/manifestor/executor_ssh.v
@@ -7,19 +7,39 @@ struct ExecutorSSH{
 	mut:
 		ipaddr 		  IPAddress
 		sshkey        string = "~/.ssh/id_rsa" // path
+		user		  string
+		initialized   bool
+
 	retry  int = 5 //nr of times something will be retried before failing, need to check also what error is, only things which should be retried need to be done
 }
 
 fn (mut executor ExecutorSSH) init(retry int) ExecutorLocal{
 		mut local_executor := ExecutorLocal{retry: retry}
-		// todo : don't load if already running
-		local_executor.exec("eval `ssh-agent -s`")
-		local_executor.exec("ssh-add $executor.sshkey")
+		if !executor.initialized{
+			// todo : don't load if already running
+			
+			local_executor.exec("pgrep -x ssh-agent || eval `ssh-agent -s`") or {panic(err)}
+			local_executor.exec("ssh-add $executor.sshkey")
+			
+			mut addr := executor.ipaddr.addr
+			if addr == ""{
+				addr = "localhost"
+			}
+
+			mut fingerprint := ""
+			fingerprint = local_executor.exec("sh -c 'ssh-keyscan -H $executor.ipaddr.addr -p $executor.ipaddr.port.number -t ecdsa-sha2-nistp256 2>/dev/null >> ~/.ssh/known_hosts'") or {panic(err)}
+			// local_executor.exec('echo "$fingerprint" ')
+			executor.initialized = true
+		}
 		return local_executor
 }
 
 pub fn (mut executor ExecutorSSH) exec(cmd string) ?string {	
 	mut local_executor := executor.init(executor.retry)
+	
+	if executor.user != ""{
+		return local_executor.exec("ssh $executor.user@$executor.ipaddr.addr -p $executor.ipaddr.port.number $cmd")
+	}
 	return local_executor.exec("ssh $executor.ipaddr.addr -p $executor.ipaddr.port.number $cmd")
 }
 
@@ -53,6 +73,9 @@ pub fn (mut executor ExecutorSSH) remove(path string) ? {
 pub fn (mut executor ExecutorSSH) download(source string, dest string) ?string {		
 	mut local_executor := executor.init(executor.retry)
 	port := executor.ipaddr.port.number
+	if executor.user != ""{
+		return local_executor.exec('rsync -avHPe "ssh -p$port" $executor.user@$executor.ipaddr.addr:$source $dest')
+	}
 	return local_executor.exec('rsync -avHPe "ssh -p$port" $executor.ipaddr.addr:$source $dest')
 }
 
@@ -60,6 +83,9 @@ pub fn (mut executor ExecutorSSH) download(source string, dest string) ?string {
 pub fn (mut executor ExecutorSSH) upload(source string, dest string) ?string {	
 	mut local_executor := executor.init(executor.retry)
 	port := executor.ipaddr.port.number
+	if executor.user != ""{
+		return local_executor.exec('rsync -avHPe "ssh -p$port" $source -e ssh $executor.user@$executor.ipaddr.addr:$dest')
+	}
 	return local_executor.exec('rsync -avHPe "ssh -p$port" $source -e ssh $executor.ipaddr.addr:$dest')
 }
 
