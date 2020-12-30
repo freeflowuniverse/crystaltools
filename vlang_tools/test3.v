a94f5bd4055401f1b50c3cee506947bd5e96ba74
Maxime Daniel <root@maxux.net>
2020-12-30 20:09:49 +0100

    vredis2: implement basic get/set commands

--- vlang_tools/test3.v
+++ vlang_tools/test3.v
@@ -3,10 +3,12 @@ import vredis2
 fn redis_server() {
 	println("Listening")
 
-	l := vredis2.listen("0.0.0.0", 5555) or { panic(err) }
+	srv := vredis2.listen("0.0.0.0", 5555) or { panic(err) }
+	mut main := &vredis2.RedisInstance{}
+
 	for {
-		conn := l.socket.accept() or { continue }
-		go vredis2.new_client(conn)
+		conn := srv.socket.accept() or { continue }
+		go vredis2.new_client(conn, mut main)
 	}
 }
 
--- vlang_tools/vredis2/vredis_server.v
+++ vlang_tools/vredis2/vredis_server.v
@@ -3,12 +3,17 @@ module vredis2
 import net
 import time
 
+pub struct RedisInstance {
+	pub mut:
+		db map[string]string
+}
+
 struct RedisSrv {
 	pub mut:
 		socket net.TcpListener
 }
 
-type RedisCallback = fn(RedisValue) RedisValue
+type RedisCallback = fn(RedisValue, mut RedisInstance) RedisValue
 
 struct RedisHandler {
 	command string
@@ -37,13 +42,20 @@ pub fn value_success(value string) RedisValue {
 	return RedisValue{datatype: RedisValTypes.success, str: value}
 }
 
+pub fn value_ok() RedisValue {
+	return value_success("OK")
+}
+
 pub fn value_error(value string) RedisValue {
 	return RedisValue{datatype: RedisValTypes.err, str: value}
 }
 
 
+//
+// commands
+//
 
-pub fn command_ping(input RedisValue) RedisValue {
+fn command_ping(input RedisValue, mut _ RedisInstance) RedisValue {
 	if input.list.len > 1 {
 		return value_success(input.list[1].str)
 	}
@@ -51,28 +63,62 @@ pub fn command_ping(input RedisValue) RedisValue {
 	return value_success("PONG")
 }
 
-pub fn process_input(mut client Redis, value RedisValue) ?bool {
+fn command_set(input RedisValue, mut srv RedisInstance) RedisValue {
+	if input.list.len != 3 {
+		return value_error("Invalid arguments")
+	}
+
+	key := input.list[1].str
+	value := input.list[2].str
+
+	srv.db[key] = value
+
+	return value_ok()
+}
+
+fn command_get(input RedisValue, mut srv RedisInstance) RedisValue {
+	if input.list.len != 2 {
+		return value_error("Invalid arguments")
+	}
+
+	key := input.list[1].str
+
+	if key !in srv.db {
+		return value_nil()
+	}
+
+	return value_str(srv.db[key])
+}
+
+//
+// socket management
+//
+pub fn process_input(mut client Redis, mut instance RedisInstance, value RedisValue) ?bool {
 	mut h := []RedisHandler{}
 
 	h << RedisHandler{command: "PING", handler: command_ping}
+	h << RedisHandler{command: "SET", handler: command_set}
+	h << RedisHandler{command: "GET", handler: command_get}
 
 	command := value.list[0].str
 
 	for rh in h {
 		if command == rh.command {
-			data := rh.handler(value)
+			println("Process: $command")
+			data := rh.handler(value, instance)
 			client.encode_send(data)
 			return true
 		}
 	}
 
+	println("Error: unknown command: $command")
 	err := value_error("Unknown command")
 	client.encode_send(err)
 
 	return false
 }
 
-pub fn new_client(conn net.TcpConn)? {
+pub fn new_client(conn net.TcpConn, mut main RedisInstance)? {
 	// create a client on the existing socket
 	mut client := Redis{socket: conn}
 
@@ -100,7 +146,7 @@ pub fn new_client(conn net.TcpConn)? {
 			return
 		}
 
-		process_input(mut client, value)?
+		process_input(mut client, mut main, value)?
 	}
 }
 
