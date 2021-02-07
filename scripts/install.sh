238b3c2d4dce08af40b89c1e9195ad73375cba15
kristof <kristof@incubaid.com>
2021-02-07 11:49:54 +0200

    isntaller

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,17 +4,19 @@ set +x
 
 # INSTALL CRYSTAL TOOLS
 
+rm -f /usr/local/bin/publishtools
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then 
     # sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/crystaluniverse/crystaltools/master/tools/install.sh)"
     echo "no release for linux yet"
     exit 1
-elif [[ "OSTYPE" == "darwin"* ]]; then
+elif [[ "$OSTYPE" == "darwin"* ]]; then
     # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/crystaluniverse/crystaltools/master/tools/install.sh)"
     curl -L https://github.com/crystaluniverse/publishtools/releases/download/first/publishtools_osx > /usr/local/bin/publishtools
 fi
 
 chmod 770 /usr/local/bin/publishtools
 
-print " - INSTALL DONE"
+echo " - INSTALL DONE"
 
 
