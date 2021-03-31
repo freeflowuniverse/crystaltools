8a07049466290628869c7eb8e92c21cb85a29a8a
despiegk <kristof@incubaid.com>
2021-03-31 19:45:45 +0400

    ...

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 set -e
 cd publishtools
 # v run publishtools.v develop
-# v -gc boehm run publishtools.v develop
-v -gc boehm run publishtools.v publish
+v -gc boehm run publishtools.v develop
+# v -gc boehm run publishtools.v publish
 # v -gc boehm -prod publishtools.v
