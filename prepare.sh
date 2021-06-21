e8b5639a5237f1eb527b69e6d8fa4da260e4b2cc
kristof de spiegeleer <kristof@greenitglobe.com>
2021-06-21 16:50:43 +0400

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,11 @@
 set -ex
-echo 1 > /tmp/log.txt
+echo 1 > log.txt
 sudo apt install redis-server -y
 bash install.sh
 bash build.sh
-echo 2 >> /tmp/log.txt
+echo 2 >> log.txt
 sudo /etc/init.d/redis-server start
-echo 3 >> /tmp/log.txt
+echo 3 >> log.txt
 # set +ex
 # publishtools flatten
 # echo 4 >> /tmp/log.txt
