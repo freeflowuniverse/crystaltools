ef762f4dd0fef5b38bc1ac4ad6affc13bef21a63
kristof de spiegeleer <kristof@greenitglobe.com>
2021-06-21 09:40:53 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # brew install libgc
     brew install bdw-gc
+else
+    sudo apt install libgc-dev
+    git clone https://github.com/vlang/v
+    cd v
+    make
+    sudo ./v symlink
 fi
 
 v install patrickpissurno.redis
