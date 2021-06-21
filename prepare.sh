94e69d5bdec2da9af246e91acb8e434382783eea
kristof de spiegeleer <kristof@greenitglobe.com>
2021-06-21 16:26:41 +0400

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -6,11 +6,11 @@ bash build.sh
 echo 2 >> /tmp/log.txt
 sudo /etc/init.d/redis-server start
 echo 3 >> /tmp/log.txt
-set +ex
-publishtools flatten
-echo 4 >> /tmp/log.txt
-publishtools flatten
-echo 5 >> /tmp/log.txt
+# set +ex
+# publishtools flatten
+# echo 4 >> /tmp/log.txt
+# publishtools flatten
+# echo 5 >> /tmp/log.txt
 
 
 
