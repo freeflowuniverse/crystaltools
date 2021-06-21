8b883582952522300577383db0945679cbb42d3e
kristof de spiegeleer <kristof@greenitglobe.com>
2021-06-21 05:55:45 +0000

    install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # brew install libgc
     brew install bdw-gc
 else
-    sudo apt install libgc-dev
+    sudo apt install libgc-dev -y
     git clone https://github.com/vlang/v
     cd v
     make
