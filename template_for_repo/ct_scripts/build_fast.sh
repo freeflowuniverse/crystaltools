6864c7a141d039d4644ebcaa9f5d10de51564761
despiegk <kristof@incubaid.com>
2021-07-27 09:24:56 +0400

    init

--- template_for_repo/ct_scripts/build_fast.sh
+++ template_for_repo/ct_scripts/build_fast.sh
@@ -1,11 +1,16 @@
 set -e
 
+export PUBLISHTOOLSBRANCH=development
+
 if [ -d "/workspace" ] 
 then
     if ! [ -d "/workspace/publishtools" ] 
     then
         pushd /workspace "$@" > /dev/null
         git clone https://github.com/crystaluniverse/publishtools
+        #which version of publish tools do you want
+        cd publishtools
+        git checkout $PUBLISHTOOLSBRANCH
         popd "$@" > /dev/null
         pushd /workspace/publishtools/scripts_workspace "$@" > /dev/null
         bash install.sh
@@ -20,6 +25,9 @@ else
     then
         pushd ~/code
         git clone https://github.com/crystaluniverse/publishtools
+        #which version of publish tools do you want
+        cd publishtools
+        git checkout $PUBLISHTOOLSBRANCH
         popd "$@" > /dev/null
         # pushd ~/code/publishtools/scripts
         # bash build_fast.sh
