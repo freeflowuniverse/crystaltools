6e01dbc5e476f011f80425976b21f3150b95969f
kristof de spiegeleer <kristof@incubaid.com>
2021-08-11 12:22:39 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 set -e
 curl https://raw.githubusercontent.com/crystaluniverse/crystaltools/development/scripts/ct_init > /tmp/init.sh
 bash /tmp/init.sh
+sudo /etc/init.d/redis-server start
 curl https://raw.githubusercontent.com/crystaluniverse/crystaltools/development/env.sh > /workspace/env.sh
 source /workspace/env.sh
 ct_build
