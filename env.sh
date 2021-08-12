d8f59d33ba38cd5b0a9ce6f8f5c4bef43abfc888
kristof de spiegeleer <kristof@incubaid.com>
2021-08-12 04:46:27 +0000

    ...

--- env.sh
+++ env.sh
@@ -5,9 +5,9 @@ then
     if [[ -z "${DIR_BIN}" ]]; then export DIR_BIN="/usr/local/bin"; fi
     if [[ -z "${DIR_BUILD}" ]]; then export DIR_BUILD="/workspace/build"; fi
     if [[ -d "/workspace/crystaltools" ]]; then
-        if [[ -z "${DIR_CT}" ]]; then export DIR_CT="/workspace/crystaltools"; fi
+        export DIR_CT="/workspace/crystaltools"
     else
-        if [[ -z "${DIR_CT}" ]]; then export DIR_CT="/workspace/code/crystaltools"; fi
+        export DIR_CT="/workspace/code/crystaltools"
     fi
     
 else
@@ -15,7 +15,7 @@ else
     if [[ -z "${DIR_CODE_INT}" ]]; then export DIR_CODE_INT="$HOME/_code"; fi
     if [[ -z "${DIR_BIN}" ]]; then export DIR_BIN="/usr/local/bin"; fi
     if [[ -z "${DIR_BUILD}" ]]; then export DIR_BUILD="/tmp"; fi
-    if [[ -z "${DIR_CT}" ]]; then export DIR_CT="$HOME/code/crystaltools"; fi
+    export DIR_CT="$HOME/code/crystaltools"
 fi
 
 export PATH=$DIR_CT/scripts:$PATH
