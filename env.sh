ff412899561603340b115d60d009d08ef9d96c03
kristof de spiegeleer <kristof@incubaid.com>
2021-08-13 10:12:38 +0000

    other dir locs

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 if [[ -d "/workspace" ]]
 then
-    if [[ -z "${DIR_CODE}" ]]; then export DIR_CODE="/workspace/code"; fi
-    if [[ -z "${DIR_CODE_INT}" ]]; then export DIR_CODE_INT="/workspace/_code"; fi
+    # if [[ -z "${DIR_CODE}" ]]; then export DIR_CODE="/workspace/code"; fi
+    # if [[ -z "${DIR_CODE_INT}" ]]; then export DIR_CODE_INT="/workspace/_code"; fi
     if [[ -z "${DIR_BIN}" ]]; then export DIR_BIN="/usr/local/bin"; fi
     if [[ -z "${DIR_BUILD}" ]]; then export DIR_BUILD="/workspace/build"; fi
     if [[ -d "/workspace/crystaltools" ]]; then
@@ -11,12 +11,14 @@ then
     fi
     
 else
-    if [[ -z "${DIR_CODE}" ]]; then export DIR_CODE="$HOME/code"; fi
-    if [[ -z "${DIR_CODE_INT}" ]]; then export DIR_CODE_INT="$HOME/_code"; fi
+    # if [[ -z "${DIR_CODE}" ]]; then export DIR_CODE="$HOME/code"; fi
+    # if [[ -z "${DIR_CODE_INT}" ]]; then export DIR_CODE_INT="$HOME/_code"; fi
     if [[ -z "${DIR_BIN}" ]]; then export DIR_BIN="/usr/local/bin"; fi
     if [[ -z "${DIR_BUILD}" ]]; then export DIR_BUILD="/tmp"; fi
     export DIR_CT="$HOME/code/crystaltools"
 fi
 
+export DIR_CODE="$HOME/code"
+export DIR_CODE_INT="$HOME/_code"
 export PATH=$DIR_CT/scripts:$PATH
 
