50e7d5ba37ff8a34641d4c1ea05b4c36158e59bc
kristof de spiegeleer <kristof@incubaid.com>
2021-08-14 08:29:15 +0000

    better env

--- env.sh
+++ env.sh
@@ -1,24 +1,24 @@
 if [[ -d "/workspace" ]]
 then
-    # if [[ -z "${DIR_CODE}" ]]; then export DIR_CODE="/workspace/code"; fi
-    # if [[ -z "${DIR_CODE_INT}" ]]; then export DIR_CODE_INT="/workspace/_code"; fi
-    if [[ -z "${DIR_BIN}" ]]; then export DIR_BIN="/usr/local/bin"; fi
-    if [[ -z "${DIR_BUILD}" ]]; then export DIR_BUILD="/workspace/build"; fi
+    export DIR_BUILD="/workspace/build"
     if [[ -d "/workspace/crystaltools" ]]; then
         export DIR_CT="/workspace/crystaltools"
     else
         export DIR_CT="/workspace/code/crystaltools"
     fi
-    
+    export DIR_BASE="/workspace/publisher"    
+    export HOME="/workspace"
 else
-    # if [[ -z "${DIR_CODE}" ]]; then export DIR_CODE="$HOME/code"; fi
-    # if [[ -z "${DIR_CODE_INT}" ]]; then export DIR_CODE_INT="$HOME/_code"; fi
-    if [[ -z "${DIR_BIN}" ]]; then export DIR_BIN="/usr/local/bin"; fi
-    if [[ -z "${DIR_BUILD}" ]]; then export DIR_BUILD="/tmp"; fi
+    export DIR_BUILD="/tmp"
+    export DIR_BASE="$HOME/.publisher"
     export DIR_CT="$HOME/code/crystaltools"
 fi
 
+
 export DIR_CODE="$HOME/code"
+export DIR_CODEWIKI="$HOME/codewiki"
 export DIR_CODE_INT="$HOME/_code"
+export DIR_BIN="/usr/local/bin"
+
 export PATH=$DIR_CT/scripts:$PATH
 
