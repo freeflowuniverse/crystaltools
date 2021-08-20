92dcffce591425cd93554d6e9ef020fb7a5e7ce0
despiegk <kristof@incubaid.com>
2021-08-20 09:49:35 +0200

    ...

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 set -ex
 sudo /etc/init.d/redis-server start
-curl https://raw.githubusercontent.com/crystaluniverse/crystaltools/development/env.sh > /workspace/env.sh
+curl https://raw.githubusercontent.com/crystaluniverse/crystaltools/development_scriptsnew/env.sh > /workspace/env.sh
 bash -ex /workspace/env.sh
 source /workspace/env.sh
 ct_build
