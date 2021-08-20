55b3d3c3071045dace4c7f3fd0c0e88b311d2cc1
kristof de spiegeleer <kristof@incubaid.com>
2021-08-20 08:17:58 +0000

    ...

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 set -ex
 sudo /etc/init.d/redis-server start
+if [[ -z "${PBRANCH}" ]]; then 
+echo " - DEFAULT BRANCH WILL BE SET."
 export PBRANCH="development_scriptsnew"
+fi
 curl https://raw.githubusercontent.com/crystaluniverse/crystaltools/$PBRANCH/env.sh > /workspace/env.sh
 bash -ex /workspace/env.sh
 source /workspace/env.sh
