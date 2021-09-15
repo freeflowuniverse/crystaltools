9f5f8907c9b37aa2ddf9a45a344a704cf9c7686b
despiegk <kristof@incubaid.com>
2021-09-15 05:36:18 +0300

    reverted changes

--- install.sh
+++ install.sh
@@ -1,16 +1,8 @@
 set -ex
 if [[ -z "${PBRANCH}" ]]; then 
-echo " - DEFAULT BRANCH WILL BE SET."
-export PBRANCH="development"
+    echo " - DEFAULT BRANCH WILL BE SET."
+    export PBRANCH="development"
 fi
-
-# #means we are in gitpod
-# if [[ -d "/workspace" ]]
-# then
-#     export PUBLISH_HOME="/workspace"
-# else
-#     export PUBLISH_HOME="$HOME"
-# fi
 
 export PUBLISH_HOME="$HOME"
 
@@ -18,7 +10,9 @@ export DIR_BASE="$PUBLISH_HOME/publisher"
 
 mkdir -p $DIR_BASE
 
+
 if [[ -f "env.sh" ]]; then 
+    #means we are working from an environment where env is already there e.g. when debug in publishing tools itself
     rm -f $PUBLISH_HOME/env.sh
     ln -sfv $PWD/env.sh $PUBLISH_HOME/env.sh 
     if [[ -d "/workspace" ]]
