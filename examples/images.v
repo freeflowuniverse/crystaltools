a1b38aefe16e68695b559daa507018461ecfdbf6
despiegk <kristof@incubaid.com>
2021-09-21 15:31:23 +0300

    refactor

---
+++ examples/images.v
@@ -0,0 +1,18 @@
+	
+	
+module main
+import despiegk.crystallib.imagemagick
+import os
+
+
+fn main() {
+	mut magic := imagemagick.new()
+	magic.add("") or {panic("did not load")}
+	// println(magic)
+
+	magic.downsize("/Users/despiegk/code/github/threefoldfoundation/info_threefold_pub/wiki/","/Users/despiegk/backup_images/threefold/") or {panic("could not downsize. $err")}
+
+
+	
+	
+}
