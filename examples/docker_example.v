4424a8a3f98f7b4e96729a04081791b2ba2e76a5
root <kristof@incubaid.com>
2022-01-12 10:15:11 +0000

    init

--- examples/docker_example.v
+++ examples/docker_example.v
@@ -1,12 +1,9 @@
 import rand
-import docker
+import despiegk.crystallib.docker
 
-fn docker1() {
-	mut engine := docker.new(
-		node_ipaddr: '174.138.48.10:22'
-		node_name: 'myremoteserver'
-		user: 'root'
-	) or { panic(err) }
+fn docker1()? {
+	sshkey := "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/9RNGKRjHvViunSOXhBF7EumrWvmqAAVJSrfGdLaVasgaYK6tkTRDzpZNplh3Tk1aowneXnZffygzIIZ82FWQYBo04IBWwFDOsCawjVbuAfcd9ZslYEYB3QnxV6ogQ4rvXnJ7IHgm3E3SZvt2l45WIyFn6ZKuFifK1aXhZkxHIPf31q68R2idJ764EsfqXfaf3q8H3u4G0NjfWmdPm9nwf/RJDZO+KYFLQ9wXeqRn6u/mRx+u7UD+Uo0xgjRQk1m8V+KuLAmqAosFdlAq0pBO8lEBpSebYdvRWxpM0QSdNrYQcMLVRX7IehizyTt+5sYYbp6f11WWcxLx0QDsUZ/J"
+	mut engine := docker.new(sshkeys_allowed=[sshkey])?
 	engine.reset_all()
 	mut containers := engine.containers_list()
 	mut images := engine.images_list()
@@ -125,6 +122,6 @@ fn docker2() {
 }
 
 fn main() {
-	// docker1()
-	docker2()
+	docker1() or { panic(err)}
+	// docker2()
 }
