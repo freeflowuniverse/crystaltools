10307bb6594977e4f547b9ac4462ae969e16e99d
despiegk <kristof@incubaid.com>
2022-01-17 23:02:07 +0400

    publishtools

---
+++ examples/wikidata/wikidata.v
@@ -0,0 +1,19 @@
+import rand
+import despiegk.crystallib.data
+
+fn data1()? {
+
+
+	url := "https://github.com/freeflowuniverse/crystaltools/tree/development/wikitest"
+
+
+	
+
+}
+
+// 	c.image.delete(false) or { panic(err) }
+// }
+
+fn main() {
+	data1() or { panic(err)}
+}
