691bca2cf869d76252a4569685da85b1d6fb7aaf
kristof de spiegeleer <kristof@incubaid.com>
2022-01-22 11:03:21 +0000

    debug ok

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -4,7 +4,7 @@ FROM gitpod/workspace-full:latest
 
 USER root
 RUN apt-get update && apt-get install -y redis-server mc
-
+RUN apt-get instal lldb -y
 
 USER gitpod
 
--- examples/taigaexport/run_production.sh
+++ examples/taigaexport/run_production.sh
@@ -1,6 +1,17 @@
 set -ex
 rm -f taiga_export_production
-v -no-parallel -d net_blocking_sockets -d static_boehm  -g -keepc  -gc boehm taiga_export_production.v
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # v -v -d static_boehm  -gc boehm -prod publishtools.v
+    v -no-parallel -d net_blocking_sockets -d static_boehm  -g -keepc  -gc boehm taiga_export_production.v
+    ulimit -n 10000
+else
+    v -no-parallel -d net_blocking_sockets -d static_boehm  -g -keepc -gc boehm taiga_export_production.v
+    # v -g -keepc -gc boehm taiga_export_production.v
+fi
+   
+
+
 # v taiga_export_production.v
-lldb taiga_export_production  --one-line r
-# publishtools develop
+# lldb taiga_export_production  --one-line r
+./taiga_export_production
