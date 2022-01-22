947a1d1b58fb3850793efd12d17f2299c5452319
despiegk <kristof@incubaid.com>
2022-01-22 14:28:15 +0400

    ...

--- examples/taigaexport/run_production.sh
+++ examples/taigaexport/run_production.sh
@@ -1,2 +1,6 @@
-v run taiga_export_production.v
+set -ex
+rm -f taiga_export_production
+v -no-parallel -d net_blocking_sockets -d static_boehm  -g -keepc  -gc boehm taiga_export_production.v
+# v taiga_export_production.v
+lldb taiga_export_production  --one-line r
 # publishtools develop
--- examples/taigaexport/taiga_export.v
+++ examples/taigaexport/taiga_export.v
@@ -3,7 +3,7 @@ import despiegk.crystallib.taiga
 // Generate wikis with users, projects, stories, issues and tasks
 fn main() {
 	url := 'https://staging.circles.threefold.me' // Add your taiga url
-	mut t := taiga.new(url, 'admin', '123123', 100000,false) // Connect with username and password and cache time in seconds
+	mut t := taiga.new(url, 'admin', '123123', 100000) // Connect with username and password and cache time in seconds
 	export_dir := '/tmp/taiga' // set export directory
 	t.cache_drop_all() or {panic(err)}
 	taiga.export(export_dir, url) or {panic(err)}
