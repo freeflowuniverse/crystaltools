d28f841fc69fb8281173e5e2b4dac9a7954d991e
despiegk <>
2022-06-18 16:57:03 +0300

    runner

--- crystaltools/runner.v
+++ crystaltools/runner.v
@@ -9,12 +9,17 @@ fn do()? {
 
 	options := cmdline.only_non_options(os.args)
 
-	if options.len != 1 {
-		println(" ERROR: please specify the path of the markdown actions file to start from")
+	if options.len != 2 {
+		// println(options)
+		return error(" ERROR: please specify the path of the markdown actions file to start from")
 	}
 	
+	if ! os.exists(options[1]){
+		return error(" ERROR: could not find path for the input actions file (md): '${options[1]}'")
+	}
+
 	//get a publisher & run it
-	mut publ := publisher_core.run(actions_path:options[0])?
+	mut _ := publisher_core.run(actions_path:options[1])?
 
 }
 
